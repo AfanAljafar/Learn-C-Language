76dcec6211dad93449dd691a8c2c7640b732b6ab
AfanAljafar <alzafarx@gmail.com>
2023-04-17 04:10:13 +0700

    learn C commit

---
+++ Modul3/modul3a.c
@@ -0,0 +1,13 @@
+//modul Do While
+#include<stdio.h>
+
+int i;
+
+void main(){
+    i=1;
+    do{
+        printf("%d \n", i);
+        i++;
+    }
+    while(i<=10);
+}
---
+++ Modul3/modul3b.c
@@ -0,0 +1,11 @@
+//modul For To Do
+
+#include<stdio.h>
+
+int i;
+
+void main(){
+    for(i=1;i<=10;i++){
+        printf("%d \n", i);
+    }
+}
