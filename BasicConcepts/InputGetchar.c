0f9029062ed7f980402c5a62b11d19bee57c769a
afan <alzafarx@gmail.com>
2023-05-12 23:48:22 +0700

    learn C

---
+++ BasicConcepts/InputGetchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    //input a single character
+    char a = getchar();
+
+    printf("you entered : %c \n", a);
+
+    return 0;
+}
---
+++ BasicConcepts/OutputPutchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    //input a single character
+    char a = getchar();
+
+    printf("you entered : ");
+
+    putchar(a);
+    printf("\n");
+
+    return 0;
+}
