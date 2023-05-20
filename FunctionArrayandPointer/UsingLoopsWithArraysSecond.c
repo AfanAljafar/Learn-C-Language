be335f7825a852735bd46546d31e0097b09e2c25
afan <alzafarx@gmail.com>
2023-05-21 03:56:39 +0700

    learn C

---
+++ FunctionArrayandPointer/UsingLoopsWithArraysSecond.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+    int k;
+
+    for (k = 0; k < 10; k++)
+    {
+        a[k] = k * 10;
+    }
+
+    for (k = 0; k < 10; k++)
+    {
+        printf("%d \n", a[k]);
+    }
+
+    return 0;
+}
