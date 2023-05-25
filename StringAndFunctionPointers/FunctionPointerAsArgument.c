040393800540039c96125ca6c2beb3b0b3cf6a5d
afan <alzafarx@gmail.com>
2023-05-26 00:37:28 +0700

    learn C

---
+++ StringAndFunctionPointers/FunctionPointerAsArgument.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *, const void *);
+
+int main()
+{
+    int arr[5] = {7, 2, 8, 6, 3};
+    int num, width, i;
+
+    num = sizeof(arr) / sizeof(arr[0]);
+    width = sizeof(arr[0]);
+    qsort((void *)arr, num, width, compare);
+    for (i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
+int compare(const void *element1, const void *element2)
+{
+    if ((*(int *)element1) == (*(int *)element2))
+        return 0;
+    else if ((*(int *)element1) < (*(int *)element2))
+        return -1;
+    else
+        return 1;
+}
