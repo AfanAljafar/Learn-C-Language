6a4a2c643df20716b8c45d873c124a423a47b2f0
afan <alzafarx@gmail.com>
2023-05-26 21:58:07 +0700

    learn C

--- step-2/Modul 2a.c
+++ step-2/Modul 2a.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-char uname[50],pass[50];
+
+char uname[50], pass[50];
+
 int main()
 {
-printf("Username : "); scanf("%s",uname);
-printf("Password : "); scanf("%s",pass);
-if (strcmp(uname,"afan")==0 && strcmp(pass,"aljafar")==0)
- {
-printf("Login Sukses \n");
-printf("Silahkan Lanjutkan\n");
- }
-else printf("Login Gagal\n");
-return 0;
+    printf("Username : ");
+    scanf("%s", uname);
+    printf("Password : ");
+    scanf("%s", pass);
+
+    if (strcmp(uname, "afan") == 0 && strcmp(pass, "aljafar") == 0)
+    {
+        printf("Login Sukses \n");
+        printf("Silahkan Lanjutkan\n");
+    }
+    else
+        printf("INVALID USERNAME or PASSWORD\n");
+    return 0;
 }
--- step-2/modul2b.c
+++ step-2/modul2b.c
@@ -1,18 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int i;
-void main(){
+
+int main()
+{
     printf("Enter the order of the day :");
     scanf("%d", &i);
-    switch(i){
-        case 1:printf("Monday");break;
-        case 2:printf("Tuesday");break;
-        case 3:printf("Wednesday");break;
-        case 4:printf("Thursday");break;
-        case 5:printf("Friday");break;
-        case 6:printf("Saturday");break;
-        case 7:printf("Sunday");break;
-        default: printf("order of days out of order"); break;
-    }
-    //return 0;
-}
 
+    switch (i)
+    {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Tuesday");
+        break;
+    case 3:
+        printf("Wednesday");
+        break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Sunday");
+        break;
+    default:
+        printf("order of days out of order");
+        break;
+    }
+
+    return 0;
+}
