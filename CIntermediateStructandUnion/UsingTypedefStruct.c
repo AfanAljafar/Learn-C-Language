678e29f85414d2ad644f94230bd1ed5e39393205
afan <alzafarx@gmail.com>
2023-06-19 23:54:01 +0700

    learn C

---
+++ CIntermediateStructandUnion/UsingTypedefStruct.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    int age;
+    char grade;
+    char name[40];
+} student;
+
+int main()
+{
+    student s1;
+    student s2;
+
+    s1.age = 17;
+    s1.grade = 'A';
+    sprintf(s1.name, "Afan Aljfar");
+
+    s2.age = 18;
+    s2.grade = 'B';
+    strcpy(s2.name, "Noir Chifuyu");
+
+    /*struct variable initialized in the declaration by listing initial values
+    in order inside curly braces*/
+    student s3 = {19, 'A', "Rimuru Tempest"};
+
+    /*initialize a structure using curly braces after declaration,
+    need to type cast, as in the statements*/
+    student s4;
+    s4 = (student){20, 'C', "Demon Diablo"};
+
+    /*use named member initialization
+    when initializing a structure to initialize corresponding members*/
+    student s5 = {.age = 21, .grade = 'A', .name = "Midoriya Izuku"};
+
+    printf("Student : \n\tName\t: %s\n\tAge\t: %d\n\tGrade\t: %c\n", s1.name, s1.age, s1.grade);
+    printf("Student : \n\tName\t: %s\n\tAge\t: %d\n\tGrade\t: %c\n", s2.name, s2.age, s2.grade);
+    printf("Student : \n\tName\t: %s\n\tAge\t: %d\n\tGrade\t: %c\n", s3.name, s3.age, s3.grade);
+    printf("Student : \n\tName\t: %s\n\tAge\t: %d\n\tGrade\t: %c\n", s4.name, s4.age, s4.grade);
+    printf("Student : \n\tName\t: %s\n\tAge\t: %d\n\tGrade\t: %c\n", s5.name, s5.age, s5.grade);
+}
